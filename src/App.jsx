1c7dde7303699a3c0d0a34437391541b94ed7bd4
Simon Tang <simon.gh.tang@gmail.com>
2023-11-08 16:01:33 +1100

    styling changes format changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,56 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(localStorage.getItem("localStorageCount") | 0);
+	const [count, setCount] = useState(localStorage.getItem("localStorageCount") | 0);
 
-  function decrementCount() {
-	const decrementedCount = getLocalStorageCount() - 1;
-	setCount(decrementedCount);
-	setLocalStorageCount(decrementedCount);
-  }
+	function decrementCount() {
+		const decrementedCount = getLocalStorageCount() - 1;
+		setCount(decrementedCount);
+		setLocalStorageCount(decrementedCount);
+	}
 
-  function incrementCount() {
-	const decrementedCount = getLocalStorageCount() + 1;
-	setCount(decrementedCount);
-	setLocalStorageCount(decrementedCount);
-  }
+	function incrementCount() {
+		const decrementedCount = getLocalStorageCount() + 1;
+		setCount(decrementedCount);
+		setLocalStorageCount(decrementedCount);
+	}
 
-  function resetCount() {
-	setCount(0);
-	setLocalStorageCount(0);
-  }
+	function resetCount() {
+		setCount(0);
+		setLocalStorageCount(0);
+	}
 
-  function clearCount() {
-	localStorage.removeItem("localStorageCount");
-  }
+	function clearCount() {
+		setCount(null);
+		localStorage.removeItem("localStorageCount");
+	}
 
-  function setLocalStorageCount(newCount) {
-	localStorage.setItem("localStorageCount", newCount);
-  }
+	function setLocalStorageCount(newCount) {
+		localStorage.setItem("localStorageCount", newCount);
+	}
 
-  function getLocalStorageCount() {
-	return parseInt(localStorage.getItem("localStorageCount"));
-  }
+	function getLocalStorageCount() {
+		return parseInt(localStorage.getItem("localStorageCount"));
+	}
 
-  return (
-    <>
-	hello world
-
-	<button onClick={decrementCount}>-</button>
-	<span>{localStorage.getItem("localStorageCount")}</span>
-	<button onClick={incrementCount}>+</button>
-	<button onClick={resetCount}>Reset Count to 0</button>
-	<button onClick={clearCount}>Clear Local Storage</button>
-    </>
-  )
+	return (
+		<div className="main-format">
+			<div className="counter-format">
+				<button onClick={decrementCount}>
+					<i className="gg-math-minus" />
+				</button>
+				<span className="counter">{localStorage.getItem("localStorageCount")}</span>
+				<button onClick={incrementCount}>
+					<i className="gg-math-plus" />
+				</button>
+			</div>
+				<button onClick={resetCount}>Reset Count to 0</button>
+			<div className="clear-local-storage-button">
+				<button onClick={clearCount}>Clear Local Storage</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
